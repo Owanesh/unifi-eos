56e0ab189c67423dbbd9a94d9c5c5d447426c9d3
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2017-06-17 11:50:40 +0200

    Source folders

---
+++ scheduler/src/placeholder.c
@@ -0,0 +1,8 @@
+/*
+ * placeholder.c
+ *
+ *  Created on: Jun 17, 2017
+ *      Author: Owanesh
+ */
+
+
---
+++ scheduler/src/placeholder.h
@@ -0,0 +1,13 @@
+/*
+ * placeholder.h
+ *
+ *  Created on: Jun 17, 2017
+ *      Author: Owanesh
+ */
+
+#ifndef PLACEHOLDER_H_
+#define PLACEHOLDER_H_
+
+
+
+#endif /* PLACEHOLDER_H_ */
